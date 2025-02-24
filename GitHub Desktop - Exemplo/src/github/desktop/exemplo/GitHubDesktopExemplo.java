9358c5e93473111f5fd19a1dd3b097ea1a8b8413
PauloCesar0709 <paulocsm2006@gmail.com>
2025-02-24 08:27:30 -0300

    Hello World

--- GitHub Desktop - Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
+++ GitHub Desktop - Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -4,17 +4,12 @@
  */
 package github.desktop.exemplo;
 
-/**
- *
- * @author Paulo
- */
+
 public class GitHubDesktopExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World"); 
     }
     
 }
