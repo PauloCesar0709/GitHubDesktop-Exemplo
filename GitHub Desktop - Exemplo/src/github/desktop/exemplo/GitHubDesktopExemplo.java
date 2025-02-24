805d2de8dc71fca412fd1b769a764cfd64840dc0
PauloCesar0709 <paulocsm2006@gmail.com>
2025-02-24 08:35:04 -0300

    Revert "Hello World"
    This reverts commit 9358c5e93473111f5fd19a1dd3b097ea1a8b8413.

--- GitHub Desktop - Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
+++ GitHub Desktop - Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -4,12 +4,17 @@
  */
 package github.desktop.exemplo;
 
-
+/**
+ *
+ * @author Paulo
+ */
 public class GitHubDesktopExemplo {
 
-  
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World"); 
+        // TODO code application logic here
     }
     
 }
